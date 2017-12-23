f968505e9f74cb0559c63a497490f61d691ba6e1
Arthur Goldman <arthur@goldman-tribe.org>
2017-12-23 15:53:36 +0100

    Respond to some signals

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ require('./app/routes.js')(app)
 
 app.listen(port)
 console.log(`listening on port ${port}`)
+
+process.on('SIGTERM', () => process.exit(0))
+
+process.on('SIGABRT', () => process.exit(0))
+
+process.on('SIGINT', () => process.exit(0))
+
+process.on('SIGQUIT', () => process.exit(0))
